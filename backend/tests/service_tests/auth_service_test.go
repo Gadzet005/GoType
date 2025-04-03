4f0fc33715a2baaa3bbb049d93f49a54a776a73c
Gerkkk <danpol2005@gmail.com>
2025-04-03 16:39:59 +0300

    mocks

--- backend/tests/service_tests/auth_service_test.go
+++ backend/tests/service_tests/auth_service_test.go
@@ -1,23 +1,23 @@
 package service_tests
 
-//import (
-//	"github.com/Gadzet005/GoType/backend/domain"
-//	mocks "github.com/Gadzet005/GoType/backend/internal/mocks/repository"
-//	service2 "github.com/Gadzet005/GoType/backend/internal/service"
-//	"github.com/stretchr/testify/assert"
-//	"github.com/stretchr/testify/mock"
-//	"testing"
-//)
-//
-//func TestCreateUser(t *testing.T) {
-//	repo := mocks.NewMockAuthorization(t)
-//	authService := service2.NewAuthService(repo)
-//
-//	repo.On("CreateUser", mock.AnythingOfType("domain.User")).Return(0, 1, "a", nil)
-//
-//	_, refresh, err := authService.CreateUser(domain.User{Password: "a", Name: "a"})
-//
-//	//asserts for valid access
-//	assert.Equal(t, "a", refresh)
-//	assert.Nil(t, err)
-//}
+import (
+	user "github.com/Gadzet005/GoType/backend/internal/domain/User"
+	service2 "github.com/Gadzet005/GoType/backend/internal/service"
+	mocks "github.com/Gadzet005/GoType/backend/tests/mocks/repository_mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+func TestCreateUser(t *testing.T) {
+	repo := mocks.NewMockAuthorization(t)
+	authService := service2.NewAuthService(repo)
+
+	repo.On("CreateUser", mock.AnythingOfType("domain.User")).Return(0, 1, "a", nil)
+
+	_, refresh, err := authService.CreateUser(user.User{Password: "a", Name: "a"})
+
+	//asserts for valid access
+	assert.Equal(t, "a", refresh)
+	assert.Nil(t, err)
+}
